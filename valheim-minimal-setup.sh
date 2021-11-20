72a72a6dec61d1ffaba7415496e23e2edd85cd87
ult0 <jabonasera@gmail.com>
2021-11-20 01:35:33 -0600

    Update valheim-minimal-setup.sh

--- valheim-minimal-setup.sh
+++ valheim-minimal-setup.sh
@@ -3,7 +3,7 @@ sudo apt -y dist-upgrade
 sudo apt-get -y install lib32gcc1
 sudo apt-get -y install libsdl2-2.0-0
 mkdir SteamCMD && cd SteamCMD
-curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_osx.tar.gz" | tar zxvf -
+curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxvf -
 /home/ubuntu/SteamCMD/steamcmd.sh +login anonymous +force_install_dir /home/ubuntu/Steam +app_update 896660 validate +exit
 cd /home/ubuntu/Steam/
 cp start_server.sh my_start_server.sh
